34457fc2766ee90326b978ff86f08d5256e6e32c
ErfanMosaddeghi <erfan.mosaddeghi@gmail.com>
2025-11-06 12:38:19 +0330

    add two practice item

---
+++ src/01.ReveresString/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = Console.ReadLine();
+
+while (string.IsNullOrWhiteSpace(input) || input.Length < 3)
+    input = Console.ReadLine();
+
+
+if (input == "0") return;
+
+Console.WriteLine(IsPalindrome(input));
+
+bool IsPalindrome(string input)
+{
+    var chars = input.ToLower().ToCharArray();
+    int i = 0, j = chars.Length - 1;
+
+    while (i < j)
+    {
+        if (chars[i] != chars[j])
+            return false;
+        i++; j--;
+    }
+    return true;
+}
---
+++ src/02.CustomeStack/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+Console.WriteLine("========");
+
+Person p1 = new Person()
+{
+    ID = 1,
+    FirstName = "item",
+    LastName = "time"
+};
+
+CustomStack<Person> stackPeople = new CustomStack<Person>();
+
+stackPeople.Push(p1);
+stackPeople.Push(new Person { ID = 2, FirstName = "test1", LastName = "1" });
+stackPeople.Push(new Person { ID = 2, FirstName = "test2", LastName = "2" });
+stackPeople.Push(new Person { ID = 2, FirstName = "test3", LastName = "3" });
+
+System.Console.WriteLine(stackPeople.Pop().FirstName);
+System.Console.WriteLine(stackPeople.Pop().FirstName);
+System.Console.WriteLine(stackPeople.Pop().FirstName);
+System.Console.WriteLine(stackPeople.Pop().FirstName);
+System.Console.WriteLine(stackPeople.Pop().FirstName);
+Console.WriteLine("========");
+
+
+class Person
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class CustomStack<T>
+{
+    private readonly List<T> _myList = new();
+
+    public void Push(T item)
+    {
+        _myList.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (_myList.Count <= 0) throw new Exception("Not in Range!");
+        var lastIndex = _myList.Count - 1;
+        var item = _myList[lastIndex];
+        _myList.RemoveAt(lastIndex);
+        return item;
+    }
+}
+
